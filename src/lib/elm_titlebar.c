318f6425a7df8fab9b8a0f4d17e229fef304caca
shilpa <shilpa.singh@samsung.com>
2010-06-16 19:11:27 +0900

    [titlebar]: standardization, code modified according to other open source widgets, label widget removed, layout replaced by edje object, changedsizehints,sub_del functions added, as well all other functions like label_sel,icon_set and end_set modified. widget checks also added.

--- src/lib/elm_titlebar.c
+++ src/lib/elm_titlebar.c
@@ -4,29 +4,32 @@
 /**
  * @addtogroup Titlebar Titlebar
  *
- * This is a titlebar. It can contain simple label and icon objects.
+ * This is a titlebar. It can contain simple label,icon and other objects.
  */
 
 typedef struct _Widget_Data Widget_Data;
 
 struct _Widget_Data
 {
-	Evas_Object *base;
-	Evas_Object *label;
+	Evas_Object *tbar;
 	Evas_Object *icon;
 	Evas_Object *end;
+	const char * label;
 };
 
+static const char *widtype = NULL;
 static void _del_hook(Evas_Object *obj);
 static void _theme_hook(Evas_Object *obj);
 static void _sizing_eval(Evas_Object *obj);
+static void _changed_size_hints(void *data, Evas *e, Evas_Object *obj, void *event_info);
+static void _sub_del(void *data, Evas_Object *obj, void *event_info);
 
 static void
 _del_hook(Evas_Object *obj)
 {
 	Widget_Data *wd = elm_widget_data_get(obj);
 	if (!wd) return;
-	if(wd->label) evas_object_del(wd->label);
+	if (wd->label) eina_stringshare_del(wd->label);
 	free(wd);
 }
 
@@ -34,14 +37,10 @@ static void
 _theme_hook(Evas_Object *obj)
 {
 	Widget_Data *wd = elm_widget_data_get(obj);
-	char buf[1024];
 	if (!wd) return;
-
-	elm_layout_theme_set(wd->base, "titlebar", "base", elm_widget_style_get(obj));
-
-	snprintf(buf, sizeof(buf), "titlebar/%s", elm_widget_style_get(obj));
-	elm_object_style_set(wd->label, buf);
-
+	_elm_theme_object_set(obj, wd->tbar, "titlebar", "base", elm_widget_style_get(obj));
+	edje_object_part_text_set(wd->tbar, "elm.text", wd->label);
+	edje_object_scale_set(wd->tbar, elm_widget_scale_get(obj) * _elm_config->scale);
 	_sizing_eval(obj);
 }
 
@@ -49,11 +48,32 @@ static void
 _sizing_eval(Evas_Object *obj)
 {
 	Widget_Data *wd = elm_widget_data_get(obj);
-	Evas_Coord minw = -1, minh = -1;
-
-	edje_object_size_min_calc(elm_layout_edje_get(wd->base), &minw, &minh);
+	Evas_Coord minw = -1, minh = -1, maxw = -1, maxh = -1;
+	if (!wd) return;
+	edje_object_size_min_calc(wd->tbar, &minw, &minh);
 	evas_object_size_hint_min_set(obj, minw, minh);
-	evas_object_size_hint_max_set(obj, -1, -1);
+	evas_object_size_hint_max_set(obj, maxw, maxh);
+}
+
+static void
+_changed_size_hints(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
+{
+   Widget_Data *wd = elm_widget_data_get(data);
+   if (!wd) return;
+   _sizing_eval(data);
+}
+
+static void
+_sub_del(void *data __UNUSED__, Evas_Object *obj, void *event_info)
+{
+   Widget_Data *wd = elm_widget_data_get(obj);
+   Evas_Object *sub = event_info;
+   if (!wd) return;
+   evas_object_event_callback_del_full(sub, EVAS_CALLBACK_CHANGED_SIZE_HINTS,
+                                  _changed_size_hints, obj);
+	if (sub == wd->icon) wd->icon = NULL;
+	else if (sub == wd->end) wd->end = NULL;
+   _sizing_eval(obj);
 }
 
 /**
@@ -74,25 +94,20 @@ elm_titlebar_add(Evas_Object *parent)
 	wd = ELM_NEW(Widget_Data);
 	e = evas_object_evas_get(parent);
 	obj = elm_widget_add(e);
+	ELM_SET_WIDTYPE(widtype, "titlebar");
 	elm_widget_type_set(obj, "titlebar");
 	elm_widget_sub_object_add(parent, obj);
 	elm_widget_data_set(obj, wd);
 	elm_widget_del_hook_set(obj, _del_hook);
 	elm_widget_theme_hook_set(obj, _theme_hook);
 
-	wd->base = elm_layout_add(obj);
-	elm_layout_theme_set(wd->base, "titlebar", "base", "default");
-	elm_widget_resize_object_set(obj, wd->base);
-	evas_object_size_hint_weight_set(wd->base, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	evas_object_size_hint_align_set(wd->base, EVAS_HINT_FILL, EVAS_HINT_FILL);
+	wd->tbar = edje_object_add(e);
+	_elm_theme_object_set(obj, wd->tbar, "titlebar", "base", "default");
+	elm_widget_resize_object_set(obj, wd->tbar);
 
-	wd->label = elm_label_add(obj);
-	elm_object_style_set(wd->label, "titlebar/default");
-
-	elm_layout_content_set(wd->base, "elm.swallow.label", wd->label);
+	evas_object_smart_callback_add(obj, "sub-object-del", _sub_del, obj);
 
 	_sizing_eval(obj);
-
 	return obj;
 }
 
@@ -107,12 +122,11 @@ elm_titlebar_add(Evas_Object *parent)
 EAPI void
 elm_titlebar_label_set(Evas_Object *obj, const char *label)
 {
+	ELM_CHECK_WIDTYPE(obj, widtype);
 	Widget_Data *wd = elm_widget_data_get(obj);
-
 	if (!wd) return;
-
-	elm_label_label_set(wd->label, label);
-
+	eina_stringshare_replace(&wd->label, label);
+	edje_object_part_text_set(wd->tbar, "elm.text", label);
 	_sizing_eval(obj);
 }
 
@@ -127,10 +141,10 @@ elm_titlebar_label_set(Evas_Object *obj, const char *label)
 EAPI const char*
 elm_titlebar_label_get(Evas_Object *obj)
 {
+	ELM_CHECK_WIDTYPE(obj, widtype) NULL;
 	Widget_Data *wd = elm_widget_data_get(obj);
 	if (!wd) return NULL;
-
-	return elm_label_label_get(wd->label);
+	return wd->label;
 }
 
 /**
@@ -144,25 +158,21 @@ elm_titlebar_label_get(Evas_Object *obj)
 EAPI void
 elm_titlebar_icon_set(Evas_Object *obj, Evas_Object *icon)
 {
+	ELM_CHECK_WIDTYPE(obj, widtype);
 	Widget_Data *wd = elm_widget_data_get(obj);
    	if (!wd) return;
    	if ((wd->icon != icon) && (wd->icon))
      		elm_widget_sub_object_del(obj, wd->icon);
-	if ((icon) && (wd->icon != icon))
-     	{
 		wd->icon = icon;
+	if (icon)
+     	{
 		elm_widget_sub_object_add(obj, icon);
-		/*
+		edje_object_part_swallow(wd->tbar, "elm.swallow.icon", icon);
 		evas_object_event_callback_add(icon, EVAS_CALLBACK_CHANGED_SIZE_HINTS,
 				       _changed_size_hints, obj);
-		*/
-		elm_layout_content_set(wd->base, "elm.swallow.icon", icon);
-		edje_object_signal_emit(elm_layout_edje_get(wd->base), "elm,state,icon,visible", "elm");
-		_sizing_eval(obj);
+		edje_object_signal_emit(wd->tbar, "elm,state,icon,visible", "elm");	
 	}
-   	else
-    		wd->icon = icon;
-
+	_sizing_eval(obj);
 }
 
 /**
@@ -176,9 +186,9 @@ elm_titlebar_icon_set(Evas_Object *obj, Evas_Object *icon)
 EAPI Evas_Object *
 elm_titlebar_icon_get(Evas_Object *obj)
 {
+	ELM_CHECK_WIDTYPE(obj, widtype) NULL;
 	Widget_Data *wd = elm_widget_data_get(obj);
    	if (!wd) return NULL;
-
 	return wd->icon;
 }
 
@@ -193,24 +203,20 @@ elm_titlebar_icon_get(Evas_Object *obj)
 EAPI void
 elm_titlebar_end_set(Evas_Object *obj, Evas_Object *end)
 {
+	ELM_CHECK_WIDTYPE(obj, widtype);
 	Widget_Data *wd = elm_widget_data_get(obj);
    	if (!wd) return;
    	if ((wd->end != end) && (wd->end))
      		elm_widget_sub_object_del(obj, wd->end);
-	if ((end) && (wd->end != end))
-     	{
 		wd->end = end;
+	if (end)
+     	{
 		elm_widget_sub_object_add(obj, end);
-		/*
+		edje_object_part_swallow(wd->tbar, "elm.swallow.end", end);
 		evas_object_event_callback_add(end, EVAS_CALLBACK_CHANGED_SIZE_HINTS,
 				       _changed_size_hints, obj);
-		*/
-		elm_layout_content_set(wd->base, "elm.swallow.end", end);
-		edje_object_signal_emit(elm_layout_edje_get(wd->base), "elm,state,end,visible", "elm");
-		_sizing_eval(obj);
 	}
-   	else
-    		wd->end = end;
+	_sizing_eval(obj);
 }
 
 /**
@@ -224,8 +230,8 @@ elm_titlebar_end_set(Evas_Object *obj, Evas_Object *end)
 EAPI Evas_Object *
 elm_titlebar_end_get(Evas_Object *obj)
 {
+	ELM_CHECK_WIDTYPE(obj, widtype) NULL;
 	Widget_Data *wd = elm_widget_data_get(obj);
    	if (!wd) return NULL;
-
 	return wd->end;
 }
