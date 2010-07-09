464fb69d6bb62841d7ad197920b8a9a854d14975
ChunEon Park <chuneon.park@samsung.com>
2010-07-09 11:44:21 +0900

    [elm_dayselector]

--- src/lib/elm_dayselector.c
+++ src/lib/elm_dayselector.c
@@ -29,6 +29,8 @@ static void _del_hook(Evas_Object *obj);
 static void _del_pre_hook(Evas_Object *obj);
 static void _theme_hook(Evas_Object *obj);
 static void _sizing_eval(Evas_Object* obj);
+//static void _dayselector_resize(void *data, Evas *e, Evas_Object *obj, void *event_info);
+//static void _changed_size_hints(void *data, Evas *e, Evas_Object *obj, void *event_info);
 
 static void
 _del_hook(Evas_Object *obj)
@@ -37,7 +39,19 @@ _del_hook(Evas_Object *obj)
    if (!wd) return;
    free(wd);
 }
+/*
+static void
+_dayselector_resize(void *data, Evas *e, Evas_Object *obj, void *event_info)
+{
+	_sizing_eval(obj);
+}
 
+static void
+_changed_size_hints(void *data, Evas *e, Evas_Object *obj, void *event_info)
+{
+	_sizing_eval(obj);
+}
+*/
 static void 
 _check_clicked(void *data, Evas_Object *obj, void *event_info)
 {
@@ -102,8 +116,27 @@ elm_dayselector_check_state_get(Evas_Object *obj, Elm_DaySelector_Day day)
 
 	return elm_check_state_get(wd->check[day]);
 }
+/*
+static void
+_sizing_eval(Evas_Object* obj)
+{
+	Evas_Coord w, h;
+	Evas_Coord min_w, min_h, max_h;
+	Widget_Data *wd = elm_widget_data_get(obj);
+	if (!wd) return;
 
+	evas_object_geometry_get(obj, NULL, NULL, &w, &h);
+	edje_object_size_min_get(wd->base, &min_w, &min_h);
+	edje_object_size_max_get(wd->base, NULL, &max_h);
 
+	fprintf( stderr, "%d %d\n", w, h );
+	if(w < min_w) w = min_w;
+	if(h < min_h) h = min_h;
+	else if(h > max_h) h = max_h;
+
+	evas_object_resize(wd->frame, w, h);  
+}
+*/
 /**
  * Add the dayselector.
  *
@@ -170,6 +203,11 @@ elm_dayselector_add(Evas_Object *parent)
 	edje_object_part_swallow(wd->base, "fri", wd->check[5]);
 	elm_check_label_set(wd->check[ELM_DAYSELECTOR_SAT], "S");
 	edje_object_part_swallow(wd->base, "sat", wd->check[6]);
+
+	//evas_object_event_callback_add(obj, EVAS_CALLBACK_RESIZE, _dayselector_resize, wd);
+	//evas_object_event_callback_add(obj, EVAS_CALLBACK_CHANGED_SIZE_HINTS, _changed_size_hints, obj);
+
+	//_sizing_eval(obj);
 
    return obj;
 }
