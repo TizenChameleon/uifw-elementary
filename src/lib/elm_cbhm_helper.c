d58a8ebf2fd3939125a67239e44284105ef00550
Hyoyoung Chang <hyoyoung.chang@samsung.com>
2010-08-12 10:35:38 +0900

    [elm_cbhm_helper] adding serial number feature

--- src/lib/elm_cbhm_helper.c
+++ src/lib/elm_cbhm_helper.c
@@ -7,7 +7,6 @@
 
 /**
  * @defgroup CBHM_helper CBHM_helper
- * @ingroup Elementary
  *
  * retrieving date from Clipboard History Manager
  * CBHM_helper supports to get CBHM's contents
@@ -24,9 +23,10 @@ static Ecore_X_Window self_win = None;
 static Eina_Bool init_flag = EINA_FALSE;
 
 void _get_clipboard_window();
+unsigned int _get_cbhm_serial_number();
 void _search_clipboard_window(Ecore_X_Window w);
 int _send_clipboard_events(char *cmd);
-int get_clipboard_data(Atom datom, char **datomptr);
+int _get_clipboard_data(Atom datom, char **datomptr);
 
 void _get_clipboard_window()
 {
@@ -45,6 +45,31 @@ void _get_clipboard_window()
 		XFree(prop_return);
 		fprintf(stderr, "## find clipboard history manager at root\n");
 	}
+}
+
+unsigned int _get_cbhm_serial_number()
+{
+	Atom actual_type;
+	int actual_format;
+	unsigned long nitems, bytes_after;
+	unsigned char *prop_return = NULL;
+	unsigned int senum = 0;
+	Atom atomCbhmSN = XInternAtom(cbhm_disp, "CBHM_SERIAL_NUMBER", False);
+
+	// FIXME : is it really needed?
+	XSync(cbhm_disp, EINA_FALSE);
+
+	if(Success == 
+	   XGetWindowProperty(cbhm_disp, cbhm_win, atomCbhmSN, 
+						  0, sizeof(Ecore_X_Window), False, XA_INTEGER, 
+						  &actual_type, &actual_format, &nitems, &bytes_after, &prop_return) && 
+	   prop_return)
+	{
+		senum = *(unsigned int*)prop_return;
+		XFree(prop_return);
+	}
+	fprintf(stderr, "## chbm_serial = %d\n", senum);
+	return senum;
 }
 
 void _search_clipboard_window(Ecore_X_Window w)
@@ -105,7 +130,7 @@ int _send_clipboard_events(char *cmd)
 	return 0;
 }
 
-int get_clipboard_data(Atom datom, char **datomptr)
+int _get_clipboard_data(Atom datom, char **datomptr)
 {
 	Atom atomUTF8String = XInternAtom(cbhm_disp, "UTF8_STRING", False);
 	Atom type;
@@ -140,7 +165,7 @@ int get_clipboard_data(Atom datom, char **datomptr)
 
 		if(propname != NULL)
 		{
-//			fprintf(stderr, "## get data : %s\n", propName);
+			fprintf(stderr, "## get data(0x%x) : %s\n", propname, propname);
 			fprintf(stderr, "## after nsize = %d\n", nsize);
 			*datomptr = propname;
 //			XFree(propName);
@@ -193,6 +218,24 @@ elm_cbhm_helper_init(Evas_Object *self)
 }
 
 /**
+ * getting serial number of CBHM
+ *
+ * @return return serial number of clipboard history manager
+ *
+ * @ingroup CBHM_helper
+ */
+EAPI unsigned int 
+elm_cbhm_get_serial_number()
+{
+	if (init_flag == EINA_FALSE)
+		return 0;
+
+	unsigned int num = 0;
+	num = _get_cbhm_serial_number();
+	return num;
+}
+
+/**
  * getting count of CBHM's contents
  *
  * @return return count of history contents
@@ -206,22 +249,24 @@ elm_cbhm_get_count()
 		return -1;
 
 	char *retptr = NULL;
+	int count = 0;
 
 	_send_clipboard_events("get count");
 
 	Atom atomCBHM_cCOUNT = XInternAtom(cbhm_disp, "CBHM_cCOUNT", False);
 
-	get_clipboard_data(atomCBHM_cCOUNT, &retptr);
+	_get_clipboard_data(atomCBHM_cCOUNT, &retptr);
 
 	if (retptr != NULL)
 	{
 		fprintf(stderr, "## c get retptr : %s\n", retptr);
+		count = atoi(retptr);
 
 		free_clipboard_data(retptr);
 		retptr = NULL;
 	}
 
-	return 0;
+	return count;
 }
 
 /**
@@ -243,7 +288,7 @@ elm_cbhm_get_raw_data()
 
 	Atom atomCBHM_cRAW = XInternAtom(cbhm_disp, "CBHM_cRAW", False);
 
-	get_clipboard_data(atomCBHM_cRAW, &retptr);
+	_get_clipboard_data(atomCBHM_cRAW, &retptr);
 
 	if (retptr != NULL)
 	{
@@ -258,18 +303,18 @@ elm_cbhm_get_raw_data()
  * getting data by history position of CBHM's contents
  * 0 is current content.
  *
- * @return return data by position of history contents
+ * @return return data pointer of position of history contents
  *
  * @ingroup CBHM_helper
  */
-EAPI int 
-elm_cbhm_get_data_by_position(int pos)
+EAPI int
+elm_cbhm_get_data_by_position(int pos, char **dataptr)
 {
 	if (init_flag == EINA_FALSE)
 		return -1;
 
 	char reqbuf[16];
-	char *retptr = NULL;
+	int ret = 0;
 	sprintf(reqbuf, "get #%d", pos);
 
 	_send_clipboard_events(reqbuf);
@@ -278,15 +323,29 @@ elm_cbhm_get_data_by_position(int pos)
 
 	Atom atomCBHM_cPOS = XInternAtom(cbhm_disp, reqbuf, False);
 
-	get_clipboard_data(atomCBHM_cPOS, &retptr);
+	ret = _get_clipboard_data(atomCBHM_cPOS, dataptr);
 
-	if (retptr != NULL)
+	if (ret >= 0 && *dataptr != NULL)
 	{
-		fprintf(stderr, "## d get retptr : %s\n", retptr);
+		fprintf(stderr, "## d get retptr : %s\n", *dataptr);
+		fprintf(stderr, "## dptr = 0x%x\n", *dataptr);
 
-		free_clipboard_data(retptr);
-		retptr = NULL;
+		return 0;
 	}
 
-	return 0;
+	return -1;
+}
+
+/**
+ * free data by history position of CBHM's contents
+ *
+ * @return None
+ *
+ * @ingroup CBHM_helper
+ */
+EAPI void
+elm_cbhm_free_data(char *dptr)
+{
+	if (dptr != NULL)
+		free_clipboard_data(dptr);
 }
