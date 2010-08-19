1e81314f1216bd3aecfdda0c996242dafa61d165
Ryuan Choi <ryuan.choi@samsung.com>
2010-08-19 13:20:10 +0900

    [elm_webview]add bounce api

--- src/lib/els_webview.h
+++ src/lib/els_webview.h
@@ -1,1 +1,2 @@
-Evas_Object* _els_webview_add(Evas_Object *parent, Eina_Bool tiled);
+Evas_Object* _elm_smart_webview_add(Evas *evas, Eina_Bool tiled);
+void         _elm_smart_webview_bounce_allow_set(Evas_Object* obj, Eina_Bool horiz, Eina_Bool vert);
