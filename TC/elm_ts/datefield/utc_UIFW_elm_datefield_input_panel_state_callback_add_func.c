6cef6c4f3c64c0af08c21dc7b0f8cd40706c9b77
Sohyun Kim <anna1014.kim@samsung.com>
2010-09-14 13:41:19 +0900

    [datefield]add testcases

---
+++ TC/elm_ts/datefield/utc_UIFW_elm_datefield_input_panel_state_callback_add_func.c
@@ -0,0 +1,108 @@
+#include <tet_api.h>
+#include <Elementary.h>
+
+// Definitions
+// For checking the result of the positive test case.
+#define TET_CHECK_PASS(x1, y...) \
+{ \
+	Evas_Object *err = y; \
+	if (err == (x1)) \
+		{ \
+			tet_printf("[TET_CHECK_PASS]:: %s[%d] : Test has failed..", __FILE__,__LINE__); \
+			tet_result(TET_FAIL); \
+			return; \
+		} \
+}
+
+// For checking the result of the negative test case.
+#define TET_CHECK_FAIL(x1, y...) \
+{ \
+	Evas_Object *err = y; \
+	if (err != (x1)) \
+		{ \
+			tet_printf("[TET_CHECK_FAIL]:: %s[%d] : Test has failed..", __FILE__,__LINE__); \
+			tet_result(TET_FAIL); \
+			return; \
+		} \
+}
+
+
+static Evas_Object *main_win;
+static Evas_Object *datefield;
+
+static void startup(void);
+static void cleanup(void);
+
+void (*tet_startup)(void) = startup;
+void (*tet_cleanup)(void) = cleanup;
+
+static void utc_UIFW_elm_datefield_input_panel_state_callback_add_func_01(void);
+static void utc_UIFW_elm_datefield_input_panel_state_callback_add_func_02(void);
+
+enum {
+	POSITIVE_TC_IDX = 0x01,
+	NEGATIVE_TC_IDX,
+};
+
+struct tet_testlist tet_testlist[] = {
+	{ utc_UIFW_elm_datefield_input_panel_state_callback_add_func_01, POSITIVE_TC_IDX },
+	{ utc_UIFW_elm_datefield_input_panel_state_callback_add_func_02, NEGATIVE_TC_IDX },
+};
+
+static void startup(void)
+{
+	tet_infoline("[[ TET_MSG ]]:: ============ Startup ============ ");
+	elm_init(0, NULL);
+	main_win = elm_win_add(NULL, "main", ELM_WIN_BASIC);
+	evas_object_show(main_win);	
+}
+
+static void cleanup(void)
+{
+	if ( NULL != datefield) {
+		evas_object_del(datefield);
+		datefield = NULL;
+	}
+	if ( NULL != main_win ) {
+		evas_object_del(main_win);
+	       	main_win = NULL;
+	}
+	elm_shutdown();
+	tet_infoline("[[ TET_MSG ]]:: ============ Cleanup ============ ");
+}
+
+static void _input_panel_state_cb(void *data, Evas_Object *obj, int value)
+{
+}
+
+/**
+ * @brief Positive test case of elm_datefield_input_panel_state_callback_add()
+ */
+static void utc_UIFW_elm_datefield_input_panel_state_callback_add_func_01(void)
+{
+	datefield = elm_datefield_add(main_win);
+	elm_datefield_input_panel_state_callback_add(datefield, _input_panel_state_cb, NULL);
+
+	/*if (!r) {
+		tet_infoline("elm_datefield_input_panel_state_callback_add() failed in positive test case");
+		tet_result(TET_FAIL);
+		return;
+	}*/
+	tet_result(TET_PASS);
+}
+
+/**
+ * @brief Negative test case of ug_init elm_datefield_input_panel_state_callback_add()
+ */
+static void utc_UIFW_elm_datefield_input_panel_state_callback_add_func_02(void)
+{
+	datefield = elm_datefield_add(main_win);
+	elm_datefield_input_panel_state_callback_add(NULL, _input_panel_state_cb, NULL);
+	
+	/*if (r) {
+		tet_infoline("elm_datefield_input_panel_state_callback_add() failed in negative test case");
+		tet_result(TET_FAIL);
+		return;
+	}*/
+	tet_result(TET_PASS);
+}
