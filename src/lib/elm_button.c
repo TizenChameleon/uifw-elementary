0f9f9d537025b05cd5b31e4fd2c1120a1310b889
Rajeev Ranjan <rajeev.r@samsung.com>
2010-10-12 12:40:50 +0900

    [src/lib/elm_button.c] Changing the default focus behavior to Set.

--- src/lib/elm_button.c
+++ src/lib/elm_button.c
@@ -315,7 +315,7 @@ elm_button_add(Evas_Object *parent)
    obj = elm_widget_add(e);
    ELM_SET_WIDTYPE(widtype, "button");
    elm_widget_type_set(obj, "button");
-   elm_widget_can_focus_set(obj, EINA_FALSE);
+   elm_widget_can_focus_set(obj, EINA_TRUE);
    elm_widget_sub_object_add(parent, obj);
    elm_widget_on_focus_hook_set( obj, _on_focus_hook, NULL );
    elm_widget_data_set(obj, wd);
