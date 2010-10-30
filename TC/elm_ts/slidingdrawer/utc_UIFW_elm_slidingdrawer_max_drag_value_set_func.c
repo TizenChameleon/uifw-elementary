6a3ebe5c0151a308dda5507a2650dd1c175220a2
ChunEon Park <chuneon.park@samsung.com>
2010-10-30 17:59:02 +0900

    [slidingdrawer] TC added

--- TC/elm_ts/slidingdrawer/utc_UIFW_elm_slidingdrawer_max_drag_value_set_func.c
+++ TC/elm_ts/slidingdrawer/utc_UIFW_elm_slidingdrawer_max_drag_value_set_func.c
@@ -28,8 +28,6 @@
 
 
 Evas_Object *main_win;
-Evas_Object *ctxpopup;
-
 
 static void startup(void);
 static void cleanup(void);
@@ -37,8 +35,7 @@ static void cleanup(void);
 void (*tet_startup)(void) = startup;
 void (*tet_cleanup)(void) = cleanup;
 
-static void utc_UIFW_elm_ctxpopup_item_label_get_func_01(void);
-static void utc_UIFW_elm_ctxpopup_item_label_get_func_02(void);
+static void utc_UIFW_elm_slidingdrawer_max_drag_value_set_func_01(void);
 
 enum {
 	POSITIVE_TC_IDX = 0x01,
@@ -46,8 +43,7 @@ enum {
 };
 
 struct tet_testlist tet_testlist[] = {
-	{ utc_UIFW_elm_ctxpopup_item_label_get_func_01, POSITIVE_TC_IDX },
-	{ utc_UIFW_elm_ctxpopup_item_label_get_func_02, NEGATIVE_TC_IDX },
+	{ utc_UIFW_elm_slidingdrawer_max_drag_value_set_func_01, POSITIVE_TC_IDX },
 	{ NULL, 0 }
 };
 
@@ -56,9 +52,7 @@ static void startup(void)
 	tet_infoline("[[ TET_MSG ]]:: ============ Startup ============ ");
 	elm_init(0, NULL);
 	main_win = elm_win_add(NULL, "main", ELM_WIN_BASIC);
-	evas_object_show(main_win);
-	ctxpopup = elm_ctxpopup_add(main_win);	
-	evas_object_show(ctxpopup);
+	evas_object_show(main_win);	
 }
 
 static void cleanup(void)
@@ -72,33 +66,14 @@ static void cleanup(void)
 }
 
 /**
- * @brief Positive test case of elm_ctxpopup_item_label_get()
+ * @brief Positive test case of elm_slidingdrawer_max_drag_value_set()
  */
-static void utc_UIFW_elm_ctxpopup_item_label_get_func_01(void)
+static void utc_UIFW_elm_slidingdrawer_max_drag_value_set_func_01(void)
 {
-	Elm_Ctxpopup_Item * item = elm_ctxpopup_item_add(ctxpopup, NULL, "TEST", NULL, NULL);
-	char *label = elm_ctxpopup_item_label_get(item);
+	Evas_Object *sd = elm_slidingdrawer_add(main_win);
+	elm_slidingdrawer_max_drag_value_set(sd, 1, 1);
+	evas_object_show(sd);
 	
-	if (strcmp( label, "TEST")) {
-		tet_infoline("elm_ctxpopup_item_label_get() failed in positive test case");
-		tet_result(TET_FAIL);
-		return;
-	}
 	tet_result(TET_PASS);
 }
 
-/**
- * @brief Negative test case of ug_init elm_ctxpopup_item_label_get()
- */
-static void utc_UIFW_elm_ctxpopup_item_label_get_func_02(void)
-{
-	Elm_Ctxpopup_Item * item = elm_ctxpopup_item_add(ctxpopup, NULL, "TEST", NULL, NULL);
-	char *label = elm_ctxpopup_item_label_get(NULL);
-	
-	if (label) {
-		tet_infoline("elm_ctxpopup_item_label_get() failed in negative test case");
-		tet_result(TET_FAIL);
-		return;
-	}
-	tet_result(TET_PASS);
-}
