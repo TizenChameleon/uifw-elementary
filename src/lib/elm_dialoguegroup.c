783295df3ac30d495cae6af5df28a9edf3282f69
WooHyun Jung <wh0705.jung@samsung.com>
2010-11-24 09:57:57 +0900

    [dialoguegroup] add expandable style

--- src/lib/elm_dialoguegroup.c
+++ src/lib/elm_dialoguegroup.c
@@ -145,6 +145,8 @@ static void _set_item_theme(Dialogue_Item *item, const char *location)
       snprintf(buf, sizeof(buf), "dataview_%s", location);
    else if (item->style == ELM_DIALOGUEGROUP_ITEM_STYLE_NO_BG) 
       snprintf(buf, sizeof(buf), "no_bg_%s", location);
+   else if (item->style == ELM_DIALOGUEGROUP_ITEM_STYLE_SUB) 
+      snprintf(buf, sizeof(buf), "sub_%s", location);
    elm_layout_theme_set(item->bg_layout, "dialoguegroup", buf, elm_widget_style_get(item->parent));
 }
 
