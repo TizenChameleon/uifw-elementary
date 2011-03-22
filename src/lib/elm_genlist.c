66e3818d73e9da9ad4b36b92432f5fa7f8d72926
Seunggyun Kim <sgyun.kim@samsung.com>
2011-03-23 02:32:28 +0900

    [elm_genlist] fix _update_job opensource bug

--- src/lib/elm_genlist.c
+++ src/lib/elm_genlist.c
@@ -2671,8 +2671,8 @@ _update_job(void *data)
         {
            if (it->updateme)
              {
-                itminw = it->w;
-                itminh = it->h;
+                itminw = it->minw;
+                itminh = it->minh;
 
                 it->updateme = EINA_FALSE;
                 if (it->realized)
