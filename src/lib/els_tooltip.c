d0ddf4bbbe4e6a7b04adb92a9311f9598294eccc
tasn <tasn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-08-29 11:55:25 +0000

    Elm tooltip: Remove the checks if object passed is an elm widget.
    This kinda reverts 62919, still kept the useful new macro/func.
    
    git-svn-id: https://svn.enlightenment.org/svn/e/trunk/elementary@62943 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- src/lib/els_tooltip.c
+++ src/lib/els_tooltip.c
@@ -738,7 +738,6 @@ elm_object_sub_tooltip_content_cb_set(Evas_Object *eventarea, Evas_Object *owner
 EAPI void
 elm_object_tooltip_show(Evas_Object *obj)
 {
-   ELM_CHECK_WID_IS(obj);
    ELM_TOOLTIP_GET_OR_RETURN(tt, obj);
    tt->visible_lock = EINA_TRUE;
    _elm_tooltip_show(tt);
@@ -756,7 +755,6 @@ elm_object_tooltip_show(Evas_Object *obj)
 EAPI void
 elm_object_tooltip_hide(Evas_Object *obj)
 {
-   ELM_CHECK_WID_IS(obj);
    ELM_TOOLTIP_GET_OR_RETURN(tt, obj);
    tt->visible_lock = EINA_FALSE;
    _elm_tooltip_hide_anim_start(tt);
@@ -777,7 +775,6 @@ elm_object_tooltip_hide(Evas_Object *obj)
 EAPI void
 elm_object_tooltip_text_set(Evas_Object *obj, const char *text)
 {
-   ELM_CHECK_WID_IS(obj);
    EINA_SAFETY_ON_NULL_RETURN(obj);
    EINA_SAFETY_ON_NULL_RETURN(text);
 
@@ -827,7 +824,6 @@ elm_object_tooltip_domain_translatable_text_set(Evas_Object *obj, const char *do
 EAPI void
 elm_object_tooltip_content_cb_set(Evas_Object *obj, Elm_Tooltip_Content_Cb func, const void *data, Evas_Smart_Cb del_cb)
 {
-   ELM_CHECK_WID_IS(obj);
    elm_object_sub_tooltip_content_cb_set(obj, obj, func, data, del_cb);
 }
 
@@ -847,7 +843,6 @@ elm_object_tooltip_content_cb_set(Evas_Object *obj, Elm_Tooltip_Content_Cb func,
 EAPI void
 elm_object_tooltip_unset(Evas_Object *obj)
 {
-   ELM_CHECK_WID_IS(obj);
    ELM_TOOLTIP_GET_OR_RETURN(tt, obj);
    _elm_tooltip_unset(tt);
 }
@@ -865,7 +860,6 @@ elm_object_tooltip_unset(Evas_Object *obj)
 EAPI void
 elm_object_tooltip_style_set(Evas_Object *obj, const char *style)
 {
-   ELM_CHECK_WID_IS(obj);
    ELM_TOOLTIP_GET_OR_RETURN(tt, obj);
    if (!eina_stringshare_replace(&tt->style, style)) return;
    elm_tooltip_theme(tt);
@@ -881,7 +875,6 @@ elm_object_tooltip_style_set(Evas_Object *obj, const char *style)
 EAPI const char *
 elm_object_tooltip_style_get(const Evas_Object *obj)
 {
-   ELM_CHECK_WID_IS(obj) NULL;
    ELM_TOOLTIP_GET_OR_RETURN(tt, obj, NULL);
    return tt->style ? tt->style : "default";
 }
@@ -929,7 +922,6 @@ elm_tooltip_delay_set(double delay)
 EAPI Eina_Bool
 elm_tooltip_size_restrict_disable(Evas_Object *obj, Eina_Bool disable)
 {
-   ELM_CHECK_WID_IS(obj) EINA_FALSE;
    ELM_TOOLTIP_GET_OR_RETURN(tt, obj, EINA_FALSE);
    return tt->free_size = disable;
 }
@@ -946,7 +938,6 @@ elm_tooltip_size_restrict_disable(Evas_Object *obj, Eina_Bool disable)
 EAPI Eina_Bool
 elm_tooltip_size_restrict_disabled_get(const Evas_Object *obj)
 {
-   ELM_CHECK_WID_IS(obj) EINA_FALSE;
    ELM_TOOLTIP_GET_OR_RETURN(tt, obj, EINA_FALSE);
    return tt->free_size;
 }
