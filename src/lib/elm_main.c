3c40d79518d5e725a6d41290efab2123934a236a
ChunEon Park <chuneon.park@samsung.com>
2011-10-19 22:28:51 +0900

    elm_main.c - merged missing APIs.

--- src/lib/elm_main.c
+++ src/lib/elm_main.c
@@ -3590,6 +3590,18 @@ elm_object_item_text_part_get(const Elm_Object_Item *it, const char *part)
    return elm_widget_item_text_part_get(it, part);
 }
 
+EAPI void *
+elm_object_item_data_get(const Elm_Object_Item *it)
+{
+   return elm_widget_item_data_get(it);
+}
+
+EAPI void
+elm_object_item_data_set(Elm_Object_Item *it, void *data)
+{
+   elm_widget_item_data_set(it, data);
+}
+
 EAPI void
 elm_object_item_signal_emit(Elm_Object_Item *it, const char *emission, const char *source)
 {
