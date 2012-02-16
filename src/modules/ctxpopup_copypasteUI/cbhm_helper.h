f236d348cb9569c15e9f11c6a5e7d683dda1afc8
deasung.kim <deasung.kim@samsung.com>
2012-02-17 01:34:31 +0900

    [Copy&Paste] remove elm_cbhm_helper.c & combine into copy&paste popup module Change-Id: Id137021b8d7cd820c17d6059517e0c10703492e8

---
+++ src/modules/ctxpopup_copypasteUI/cbhm_helper.h
@@ -0,0 +1,21 @@
+#include <Elementary.h>
+#include <Eina.h>
+#include "elm_priv.h"
+
+#ifdef HAVE_ELEMENTARY_X
+#include <Ecore_X.h>
+#endif
+
+#ifdef DEBUG
+#define DMSG(fmt, args...) printf("[%s], "fmt, __func__, ##args)
+#else
+#define DMSG(args...)
+#endif
+
+Eina_Bool _cbhm_msg_send(Evas_Object* obj, char *msg);
+int _cbhm_item_count_get(Evas_Object *obj);
+#ifdef HAVE_ELEMENTARY_X
+Eina_Bool _cbhm_item_get(Evas_Object *obj, int index, Ecore_X_Atom *data_type, char **buf);
+#else
+Eina_Bool _cbhm_item_get(Evas_Object *obj, int index, void *data_type, char **buf);
+#endif
