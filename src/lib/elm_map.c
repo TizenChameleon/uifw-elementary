18b9b67cf772e10017322471249bd788032901b3
Tae-Hwan Kim <the81.kim@samsung.com>
2012-02-22 16:39:31 +0900

    [Map] Tunnig rotate callback because of gesture layer merge
    Change-Id: I21ec1ccbabfc1a8dba848da4c4fcb4a47d272aec

--- src/lib/elm_map.c
+++ src/lib/elm_map.c
@@ -2943,7 +2943,7 @@ rotate_cb(void *data, void *event_info)
         Elm_Gesture_Rotate_Info *ei = event_info;
         evas_object_geometry_get(wd->obj, &x, &y, &w, &h);
 
-        wd->rotate.d = wd->rotate.a + (ei->base_angle-ei->angle)*50;
+        wd->rotate.d = wd->rotate.a + ei->angle - ei->base_angle;
         wd->rotate.cx = x + ((double)w * 0.5);
         wd->rotate.cy = y + ((double)h * 0.5);
 
