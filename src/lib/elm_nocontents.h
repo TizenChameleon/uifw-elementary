bc01a89add28de568c9bcc165cc008a19a48312d
Jeonghyun Yun <jh0506.yun@samsung.com>
2012-03-08 14:26:45 +0900

    deleted and modified

--- src/lib/elm_nocontents.h
+++
@@ -1,7 +0,0 @@
-   /* NoContents */
-   EAPI Evas_Object *elm_nocontents_add(Evas_Object *parent);
-   EAPI void         elm_nocontents_label_set(Evas_Object *obj, const char *label);
-   EAPI const char  *elm_nocontents_label_get(const Evas_Object *obj);
-   EAPI void         elm_nocontents_custom_set(const Evas_Object *obj, Evas_Object *custom);
-   EAPI Evas_Object *elm_nocontents_custom_get(const Evas_Object *obj);
-
